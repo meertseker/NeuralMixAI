1feb1b66b36c603d864601e5323556c4001d37c3
Senin Adın <senin_email_adresin@example.com>
2025-02-21 18:30:56 +0000

    dashboard layout applied

--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -7,7 +7,15 @@ const Dashboard = () => {
   return (
     <div className='dashboard'>
       <Sidebar />
-      {/* Other components and content */}
+      <div className="dashboard-main">
+        <div className="dashboard-card">
+          <h2>Create a Mix Vocal Chain</h2>
+          <p>Enter either beat or desciption.</p>
+        </div>
+        <div className="dashboard-input"></div>
+        <div className="dashboard-settings"></div>
+        <div className="dashboard-generate-button"><p>Generate Vocal Chain</p></div>
+      </div>
     </div>
   );
 };
