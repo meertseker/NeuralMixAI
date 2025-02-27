aafb1bb07b604a66be3b40c517a8197ba6f8c591
ali arhan <mac@macs-MacBook-Pro.local>
2025-02-27 22:02:46 +0300

    mic logo is added

--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -14,7 +14,12 @@ const Dashboard = () => {
           <h2>Create a Mix Vocal Chain</h2>
           <p>Enter either beat or desciption.</p>
         </div>
-        <div className="dashboard-input"></div>
+        <div className="dashboard-input">
+            <img src="mLogo.png" alt="microphoneImage"/>
+
+
+
+        </div>
         <div className="dashboard-settings"></div>
         <div className="dashboard-generate-button"><p>Generate Vocal Chain</p></div>
       </div>
