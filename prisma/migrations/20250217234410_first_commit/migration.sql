c9348dbb61c9ab68471d16ba2b5cdffb92799e4a
Senin Adın <senin_email_adresin@example.com>
2025-03-06 19:52:16 +0000

    front back layout

--- prisma/migrations/20250217234410_first_commit/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "VocalChain" (
-    "id" SERIAL NOT NULL,
-    "userId" INTEGER NOT NULL,
-    "vocalChainName" TEXT NOT NULL,
-
-    CONSTRAINT "VocalChain_pkey" PRIMARY KEY ("id")
-);
