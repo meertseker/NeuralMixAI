8d7c6e995f52e9ff7a46475657098c25f1ac6c22
Senin Adın <senin_email_adresin@example.com>
2025-04-26 20:34:25 +0100

    email newsletter working now

--- frontend/src/intro/components/Waitlist.js
+++ frontend/src/intro/components/Waitlist.js
@@ -19,14 +19,14 @@ const WaitlistCTA = () => {
     try {
       setIsSubmitting(true);
       setSubmitStatus('Submitting...');
-      const waitlistApiUrl = await fetch(process.env.WAITLIST-API)
-      const response = await fetch(waitlistApiUrl, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email }),
-      });
+      const waitlistApiUrl = process.env.REACT_APP_WAITLIST_API;
+    const response = await fetch(waitlistApiUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email }),
+    });
 
       if (response.ok) {
         const data = await response.json(); 
